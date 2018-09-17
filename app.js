7fd13ea6398deaddafb469806ba4b7a61e89ee59
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-16 17:11:35 -0700

    Add dotenv

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 
 
 const app = express();

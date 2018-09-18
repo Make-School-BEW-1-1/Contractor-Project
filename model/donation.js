9f2e2372fac477faec1fc2a08af9b9b2ebfe8b57
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-17 20:40:32 -0700

    Connect charity to donation document, allow for creation of donation from within charity search

--- model/donation.js
+++ model/donation.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 module.exports = mongoose.model('Donation', {
-    charity: String,
+    charity: {
+        charityName: String,
+        currentRating: {
+            rating: Number
+        },
+        tagline: String,
+        websiteURL: String
+    },
     amount: Schema.Types.Decimal128,
     date: String
 });
